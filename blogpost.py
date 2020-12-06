cf0c330ee9f26113709ef39eb06aa66dd37aea4b
Eshel <eshel>
2020-12-07 01:00:57 +0200

    implemented bunch functionality in get response

--- blogpost.py
+++ blogpost.py
@@ -3,6 +3,7 @@ from flask import make_response, abort
 from bootstrap import db
 from models.blogpost_model import BlogPost, BlogPostSchema
 from sqlalchemy import func
+from utils import static_var
 from configuration import get_posts_size
 
 
@@ -25,6 +26,7 @@ def create_new_post(blogpost):
 
 
 # handler for GET /posts
+@static_var("current_offset", 0)
 def get_bunch_of_posts():
     """
     Responds to a request for GET /blogsposts
@@ -32,8 +34,15 @@ def get_bunch_of_posts():
     creation time
     """
     # TODO record runtime
-    blogposts = BlogPost.query.order_by(BlogPost.creation_timestamp).all()
-    print(blogposts)
+    get_bunch_of_posts.current_offset += get_posts_size
+    if get_bunch_of_posts.current_offset >= number_of_posts():
+        get_bunch_of_posts.current_offset = 0
+
+    blogposts_query = BlogPost.query.order_by(BlogPost.creation_timestamp)
+    blogposts_query = blogposts_query.limit(get_posts_size)
+    blogposts_query = blogposts_query.offset(get_bunch_of_posts.current_offset)
+    blogposts = blogposts_query.all()
+
     for i in blogposts:
         print(i, i.id, i.title, i.creator, i.body)
 
@@ -50,10 +59,12 @@ def get_number_of_posts():
     Responds to a request for GET /postsnumber
     :return:        number of posts in the system
     """
-    print("entering get number of posts function")
-    query_result = db.session.query(func.count(BlogPost.id)).first()
-    number_of_posts = query_result[0]
-    print("number of posts:", number_of_posts)
     return make_response(
-        f"there are currently {number_of_posts} posts in the system", 200
+        f"the total number of posts is: {number_of_posts()}", 200
     )
+
+
+def number_of_posts():
+    query_result = db.session.query(func.count(BlogPost.id)).first()
+    return int(query_result[0])
+
---
+++ utils.py
@@ -0,0 +1,7 @@
+def static_var(varname, value):
+    def decorate(func):
+        setattr(func, varname, value)
+        return func
+    return decorate
+
+
