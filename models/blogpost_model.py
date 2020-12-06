9cdc5c93bac6f679ea0f3aaceba79a40054d3b26
Eshel <eshel>
2020-12-06 20:53:51 +0200

    added models for two tables: blogposts, and runtimestats, as well as serializers

---
+++ models/blogpost_model.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from config import db, ma
+
+
+class BlogPost(db.Model):
+    __tablename__ = 'blogpost'
+
+    id = db.Column(db.Integer, primary_key='True')
+    title = db.Column(db.String)
+    body = db.Column(db.String)
+    creator = db.Column(db.String)
+    creation_timestamp = db.Column(db.String, default=datetime.utcnow)
+    
+
+class BlogPostSchema(ma.ModelSchema):
+    class Meta:
+        model = BlogPost
+        sqla_session = db.session
---
+++ models/stats_model.py
@@ -0,0 +1,16 @@
+from config import db, ma
+
+
+class RuntimeStats(db.Model):
+    __tablename__ = 'runtimestats'
+
+    id = db.Column(db.Integer, primary_key='True')
+    function_name = db.Column(db.String)
+    runtime_avg = db.Column(db.Float)
+    call_times = db.Column(db.Integer)
+    
+
+class RuntimeStatsSchema(ma.ModelSchema):
+    class Meta:
+        model = RuntimeStats
+        sqla_session = db.session
