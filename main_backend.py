55d3828750863f4b8acfa35ff2e357ad41217bc1
Eshel <eshel>
2020-12-06 20:38:49 +0200

    main_backend created which takes into account the SQLalchemy+flask structure of the project

---
+++ main_backend.py
@@ -0,0 +1,31 @@
+import connexion
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+# variables to export:
+basedir = ""
+flask_app = None
+db = None
+ma = None
+
+
+def initialize_backend():
+    global basedir
+    global flask_app
+    global db
+    global ma
+
+    basedir = os.path.abspath('.')
+
+    swagger_app = connexion.App(__name__, specification_dir=basedir)
+    flask_app = swagger_app.app
+
+    flask_app.config['SQLALCHEMY_ECHO'] = True
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + \
+        os.path.join(basedir, 'blogposts.db')
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db = SQLAlchemy(flask_app)
+
+    ma = Marshmallow(flask_app)
