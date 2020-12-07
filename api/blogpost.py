b2f4264ba7561ed19cd8dd881aea813970ab8358
Eshel <eshel>
2020-12-07 11:17:51 +0200

    moved the get number of posts function to utils file, since it's not part of the api and used by multiple other functions. also updated comments

--- api/blogpost.py
+++ api/blogpost.py
@@ -2,23 +2,22 @@ from flask import make_response
 from bootstrap import db
 from models.blogpost_model import BlogPost, BlogPostSchema
 from sqlalchemy import func, desc
-from utils import static_var
+from utils import static_var, number_of_posts
 from project_configuration.configuration import get_posts_size, top_creators_limit
 from api.runtimestats import RuntimeRecorder
 import json
 
 
-# handler for POST /post
 def create_new_post(blogpost):
     """
     Creates new blogpost based on user's request for POST /blogsposts
-    Note that input validation is done automatically by swagger, by the standards defined in api_configuration.yml
+    Note that input validation is done automatically by swagger,
+    by the standards defined in api_configuration.yml
+    Note: this function's runtime is recorded and saved to the DB for stats querying
     """
     with RuntimeRecorder('create_new_post'):
         schema = BlogPostSchema()
-        print("the json blogpost that came from user is:", blogpost)
         new_blogpost = schema.load(blogpost, session=db.session)
-        print(new_blogpost)
 
         db.session.add(new_blogpost)
         db.session.commit()
@@ -26,15 +25,17 @@ def create_new_post(blogpost):
         return make_response(f"blogpost successfully created", 201)
 
 
-# handler for GET /posts
 @static_var("current_offset", 0)
 def get_bunch_of_posts():
     """
     Responds to a request for GET /blogsposts
+    Note: this function's runtime is recorded and saved to the DB for stats querying
     :return:        list of a predetermined size of blogposts, sorted by
     creation time
     """
     with RuntimeRecorder('get_bunch_of_posts'):
+        # remember the indexes of the last bunch we sent,
+        # to be able to send the next bunch
         get_bunch_of_posts.current_offset += get_posts_size
         if get_bunch_of_posts.current_offset >= number_of_posts():
             get_bunch_of_posts.current_offset = 0
@@ -44,50 +45,37 @@ def get_bunch_of_posts():
         blogposts_query = blogposts_query.offset(get_bunch_of_posts.current_offset)
         blogposts = blogposts_query.all()
 
-        # TODO currently sends all posts, send only a bunch
         blogpost_schema = BlogPostSchema(many=True)
         data = blogpost_schema.dump(blogposts)
 
         return data
 
 
-# handler for GET /postsnumber
 def get_number_of_posts():
     """
     Responds to a request for GET /postsnumber
     :return:        number of posts in the system
     """
-    return make_response(
-        f"the total number of posts is: {number_of_posts()}", 200
-    )
+    return make_response(f"the total number of posts is: {number_of_posts()}", 200)
 
 
-def number_of_posts():
-    query_result = db.session.query(func.count(BlogPost.id)).first()
-    return int(query_result[0])
-
-
-# handler for GET /postsnumber
 def get_top_creators():
-    """
-    Responds to a request for GET /postsnumber
-    :return:        number of posts in the system
+    f"""
+    Responds to a request for GET /topcreators
+    :return:        list of size {top_creators_limit} of the creators with the most posts
     """
     query = db.session.query(BlogPost.creator, func.count(BlogPost.creator))
     query = query.group_by(BlogPost.creator)
     query = query.order_by(desc(func.count(BlogPost.creator)))
     query_results = query.limit(top_creators_limit).all()
 
-    # convert results from a list of values to a list of dictionaries with
-    # titles to each value, same format as with posts
+    # since there is no schema for creators, convert results to the same format as with posts.
+    # [(name, number)] -> [{"creator": name, "number": number}]
     results = []
     for query_result in query_results:
         results += [{"creator": query_result[0],
-                    "number_of posts:": query_result[1]}]
+                     "number_of posts:": query_result[1]}]
 
     data = json.dumps(results, indent=4)
 
-    return make_response(
-        data, 200
-    )
-
+    return make_response(data, 200)
--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
-def static_var(varname, value):
-    def decorate(func):
-        setattr(func, varname, value)
-        return func
+from sqlalchemy import func
+from bootstrap import db
+from models.blogpost_model import BlogPost
+
+
+def static_var(variable_name, value):
+    def decorate(function):
+        setattr(function, variable_name, value)
+        return function
+
     return decorate
+
+
+def number_of_posts():
+    query_result = db.session.query(func.count(BlogPost.id)).first()
+    return int(query_result[0])
